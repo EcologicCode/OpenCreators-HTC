c78fe8619bce3cc0a64d9ab08c6d4760bad81c1b
SuperAtraction <91375731+SuperAtraction@users.noreply.github.com>
2022-04-22 19:40:02 +0200

    Update index.php

--- Php/index.php
+++ Php/index.php
@@ -1,1 +1,17 @@
-<link rel="stylesheet" type="text/css" href="css/base.css">
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/base.css">
+	<title>Hyper Text Preprocessor Plug-in in Php for HTC - Editeur</title>
+</head>
+<body>
+<header style="background-color: rgb(70, 70, 70);">
+	<p>
+		<a href="../index.php?Sess=true">< Retour</a><br>
+		<a href="../../index.php">< Finir</a>
+	</p>
+</header>
+</body>
+</html>
