ec80150d6a751eee3724dca3e4c4b4b501d4e71b
SuperAtraction <91375731+SuperAtraction@users.noreply.github.com>
2022-04-23 14:08:09 +0200

    Update index.php

--- Php/index.php
+++ Php/index.php
@@ -17,14 +17,17 @@ if($_SESSION['Accept'] == true): ?>
 		<a href="../index.php">&lt; Finir</a>
 	</p>
 </header>
+<font size="3">
 <center>
 	<h1>OpenCreators : HTC<br>Hyper Text Preprocessor Plug-in in Php for HTC</h1>
 	<p>
 		Bienvenue sur Hyper Text Preprocessor Plug-in in Php for HTC !<br>
 		Autrement : HTPPIP<br>
 		Ce système a été consus pour faciliter la création de sites Webs<br>
+		<?php if(isset($_SESSION['1Load'])){echo "<a href='edit.php?f=index'>Démmarer maintenant</a><br>"; unset($_SESSION['1Load']);}?>
 	</p>
 </center>
+</font>
 </body>
 </html>
 <?php 
